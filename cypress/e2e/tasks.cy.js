6e24de81a9485c9a228ceeee342d71e91ee5946d
Matheus Martins <matheuss_fernandes@outlook.com>
2024-07-16 01:46:26 -0300

    .

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,9 +1,18 @@
 /// <reference types="Cypress" />
 
+import { faker } from '@faker-js/faker';
+
 describe('tarefas', () => {
     it('deve cadastrar uma nova tarefa', () => {
         cy.visit('http://localhost:8080/');
-        cy.get('input[placeholder="Add a new Task"]').type('hello world')
-        cy.contains('button', 'Create').click()
+        cy.get('input[placeholder="Add a new Task"]').type(faker.music.genre());
+
+        cy.intercept('POST', '**/tasks').as('postTask');
+        cy.contains('button', 'Create').click();
+
+        cy.wait('@postTask').then(xhr => {
+            expect(xhr.response.statusCode).to.eq(201)
+        });
+
     });
 });
